9382ad250b24633337bf53f69c22cbd15406c2a4
Riche0826 <34505961+Riche0826@users.noreply.github.com>
2019-06-22 00:30:16 +0800

    Update MainActivity.java

--- app/src/main/java/flag/com/tw/ch04_ezcounter/MainActivity.java
+++ app/src/main/java/flag/com/tw/ch04_ezcounter/MainActivity.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
     TextView txv;
-    Button addBtn;
+    Button addBtn, subtractBtn, endBtn;
     int counter = 1, i;
 
     @Override
@@ -19,14 +19,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         txv = (TextView) findViewById(R.id.txv);
         addBtn = (Button) findViewById(R.id.addBtn);
+        subtractBtn = (Button) findViewById(R.id.subtractBtn);
+        endBtn = (Button) findViewById(R.id.endBtn);
+
+        //監聽addBtn, subtractBtn, endBtn有無click動作
         addBtn.setOnClickListener(this);
+        subtractBtn.setOnClickListener(this);
+        endBtn.setOnClickListener(this);
+
+        //監聽addBtn, txv有無長按動作
+        addBtn.setOnLongClickListener(this);
+        txv.setOnLongClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        counter += counter;
+    public void onClick(View v) {
+        if(v.getId() == R.id.addBtn){
+            counter++;
 
-        txv.setText(String.valueOf(counter));
-        addBtn.setText("加" + counter);
+            txv.setText(String.valueOf(counter));
+        }else if(v.getId() == R.id.subtractBtn){
+            counter--;
+
+            txv.setText(String.valueOf(counter));
+        }else if(v.getId() == R.id.endBtn) finish();//finish 結束程式
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if(v.getId() == R.id.txv){
+            counter = 0;
+            txv.setText("0");
+        }else{
+            counter += 2;
+            txv.setText(String.valueOf(counter));
+        }
+
+        return true;
     }
 }
